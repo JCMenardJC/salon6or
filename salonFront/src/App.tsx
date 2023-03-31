d868b5ae78262981b8e15cf2ea328afaea7cd24a
Titou <menardjc.33@gmail.com>
2023-03-31 15:39:26 +0200

    feat carroussel photos de coiffures

--- salonFront/src/App.tsx
+++ salonFront/src/App.tsx
@@ -1,15 +1,52 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import CoiffureFemme from "./components/coiffureFemme/coiffureFemme";
+import Accueil from "./components/coiffureFemme/coiffureFemme";
+import CoiffureHomme from "./components/coiffureHomme/coiffureHomme";
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="cs-text">
-        <span className="cs-text-cut">Smooth</span>
-        <span className="cs-text-mid">Operator</span>
-        <span className="cs-text-cut">Smooth</span>
-      </h2>
+    <div className="App vert h-50">
+      <img src="logo.png" className="img-fluid" alt="logo"></img>
+      <nav className="navbar navbar-expand-lg vert">
+        <div className="container-fluid text-center">
+          <a className="navbar-brand nav" href="#">
+            Accueil
+          </a>
+          <button
+            className="burger navbar-toggler w-25 h-25"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="">
+              <img src="menu.ico" className="w-25 h-25" alt="menu"></img>
+            </span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link nav" aria-current="page" href="#">
+                Coiffures Femmes
+              </a>
+              <a className="nav-link nav" href="#">
+                Coiffures Hommes & Barbe
+              </a>
+              <a className="nav-link nav" href="#">
+                Prestations
+              </a>
+              <a className="nav-link nav" href="#">
+                Contact & Rendez-vous
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="main">
+        {/*  <CoiffureFemme /> */}
+        <CoiffureHomme />
+      </div>
     </div>
   );
 }
---
+++ salonFront/src/components/coiffureFemme/coiffureFemme.tsx
@@ -0,0 +1,93 @@
+import "./coiffureFemme.css";
+function CoiffureFemme() {
+  return (
+    <div className="content">
+      <div className="wrapper">
+        <ul className="stage">
+          <li className="scene">
+            <div className="movie">
+              <div className="poster"></div>
+              <div className="info">
+                <header>
+                  <h1>La Couleur</h1>
+                  <span className="year">A partir de 45€ </span>
+                  <div></div>
+                  <span className="duration"> 45 minutes</span>
+                </header>
+                <p>
+                  La coloration des cheveux ou plus simplement coloration,
+                  incluant aussi la teinture des cheveux, désigne la pratique
+                  cosmétique du changement de la couleur des cheveux.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="scene">
+            <div className="movie">
+              <div className="poster"></div>
+              <div className="info">
+                <header>
+                  <h1>La Coiffure Cocooning</h1>
+                  <span className="year">1946</span>
+                  <span className="rating">PG</span>
+                  <span className="duration">130 minutes</span>
+                </header>
+                <p>
+                  In Bedford Falls, New York on Christmas Eve, George Bailey is
+                  deeply troubled. Prayers for his well-being from friends and
+                  family reach Heaven. Clarence Odbody, Angel Second Class, is
+                  assigned to visit Earth to save George, thereby earning his
+                  wings. Franklin and Joseph, the head angels, review George's
+                  life with Clarence.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="scene">
+            <div className="movie">
+              <div className="poster"></div>
+              <div className="info">
+                <header>
+                  <h1>La Coiffure Cocooning</h1>
+                  <span className="year">1946</span>
+                  <span className="rating">PG</span>
+                  <span className="duration">130 minutes</span>
+                </header>
+                <p>
+                  In Bedford Falls, New York on Christmas Eve, George Bailey is
+                  deeply troubled. Prayers for his well-being from friends and
+                  family reach Heaven. Clarence Odbody, Angel Second Class, is
+                  assigned to visit Earth to save George, thereby earning his
+                  wings. Franklin and Joseph, the head angels, review George's
+                  life with Clarence.
+                </p>
+              </div>
+            </div>
+          </li>
+          <li className="scene">
+            <div className="movie">
+              <div className="poster"></div>
+              <div className="info">
+                <header>
+                  <h1>La Couleur</h1>
+                  <span className="year">A partir de 45€</span>
+                  <span className="rating"></span>
+                  <span className="duration">45 minutes</span>
+                </header>
+                <p>
+                  In Bedford Falls, New York on Christmas Eve, George Bailey is
+                  deeply troubled. Prayers for his well-being from friends and
+                  family reach Heaven. Clarence Odbody, Angel Second Class, is
+                  assigned to visit Earth to save George, thereby earning his
+                  wings. Franklin and Joseph, the head angels, review George's
+                  life with Clarence.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default CoiffureFemme;
