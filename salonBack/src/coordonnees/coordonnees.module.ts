2b6a5e40dfb819c36ba3985ce8f0ebc170d8b0ba
Titou <menardjc.33@gmail.com>
2023-04-18 10:04:10 +0200

    feat : users

--- salonBack/src/coordonnees/coordonnees.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CoordonneesService } from './coordonnees.service';
-import { CoordonneesController } from './coordonnees.controller';
-
-@Module({
-  controllers: [CoordonneesController],
-  providers: [CoordonneesService]
-})
-export class CoordonneesModule {}
--- salonBack/src/coordonnees/dto/update-coordonnee.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCoordonneeDto } from './create-coordonnee.dto';
-
-export class UpdateCoordonneeDto extends PartialType(CreateCoordonneeDto) {}
